572e9e328879c41c9d57107129e9f13ab26b7252
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2018-04-13 11:03:55 -0400

    sorting algorithm completed- project with Cassie.

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -9,14 +9,12 @@
 import Foundation
 
 // --------------- Convenience functions ---------------
-
 // Generates a random number between the minimum and maximum values given (inclusive)
 func random(between min: UInt32, and max: UInt32) -> Int {
     return Int(arc4random_uniform(max - min + 1) + min)
 }
 
 // ----------- Start of main part of program -----------
-
 // Ask the user if they are ready to begin
 print("Press any key to generate the unsorted list.", terminator: "")
 
@@ -24,29 +22,71 @@ print("Press any key to generate the unsorted list.", terminator: "")
 let input = readLine()
 
 // Make an empty array / list
-var numbers : [Int] = []
+var unsorted : [Int] = []
 
 // Populate the array
-for _ in 1...10 {
-    numbers.append( random(between: 1, and: 50) )
-}
+//for _ in 1...10 {
+//    unsorted.append( random(between: 1, and: 50) )
+//}
+unsorted.append(8)
+unsorted.append(7)
+unsorted.append(3)
+unsorted.append(4)
+unsorted.append(5)
+unsorted.append(6)
 
 // Show the user the contents of the array
 print("-----")
 print("The contents of the unsorted list:")
-for i in 0...numbers.count - 1 {
-    print("Index \(i), value: \(numbers[i])")
+for i in 0...unsorted.count - 1 {
+    print("Index \(i), value: \(unsorted[i])")
 }
 print("-----")
 
 // ----------- Write code to sort the array here -----------
 print("Now sorting the array...")
 
+// Create an empty sorted list
+var sorted : [Int] = []
+
+// Sort the unsorted list
+while unsorted.count > 0 {
+    
+    // Take a number off the end of the unsorted list
+    let numberToBeInserted = unsorted[unsorted.count - 1]
+    unsorted.remove(at: unsorted.count - 1)   // Remove number at end of the array
+    
+    // How many cards are in the sorted list?
+    if sorted.count == 0  {
+        // If there were none, just put the first card into the array
+        sorted.append(numberToBeInserted)
+    } else {
+        // Find where to insert the new number in the sorted list
+        var numberWasInserted = false
+        for i in 0...sorted.count - 1 {
+            if sorted[i] > numberToBeInserted{
+                sorted.insert(numberToBeInserted, at: i)
+                numberWasInserted = true
+                break // we've insered the number in the sorted list, now stop this loop
+            }
+        }
+        
+        // Add a card to the end of the sorted list only when it was not already inserted
+        if numberWasInserted == false {
+            sorted.append(numberToBeInserted)
+        }
+        
+    }
+    
+    
+}
+
+
 // ----------- Final part of the program, show the sorted list -----------
 print("-----")
 print("The contents of the sorted list:")
-for i in 0...numbers.count - 1 {
-    print("Index \(i), value: \(numbers[i])")
+for i in 0...unsorted.count - 1 {
+    print("Index \(i), value: \(unsorted[i])")
 }
 print("-----")
 
